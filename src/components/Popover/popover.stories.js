dabc9ec19bc1df2c5e3e870aba3bcae6fb9dbfb8
kseniya <kaprisa57@gmail.com>
2020-12-07 10:53:45 +0300

    add animation object to storybook

--- src/components/Popover/popover.stories.js
+++ src/components/Popover/popover.stories.js
@@ -198,9 +198,14 @@ export default {
       },
     },
     animation: {
-      disable: true,
+      control: 'object',
       description:
         'framer-motion props for opening/closing content animation {initial, animate, exit}',
+      defaultValue: {
+        initial: { opacity: 0, scale: 0.9 },
+        animate: { opacity: 1, scale: 1 },
+        exit: { opacity: 0, scale: 0.9, transition: { duration: 0.1 } }
+      },
       table: {
         defaultValue: {
           summary: `{
